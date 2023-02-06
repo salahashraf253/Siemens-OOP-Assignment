20ad6bf1d21471d866b5775710f765bdcafba8f4
Salah Ashraf <salahashraf924@gmail.com>
2023-02-07 00:05:00 +0200

    Create FileReader.h

---
+++ FileReader.h
@@ -0,0 +1,17 @@
+#include <fstream>
+#include <string>
+
+class FileReader
+{
+private:
+    std::string fileName;
+    std::ifstream fileStream;
+
+public:
+    void open();
+    bool isGood();
+    std::string readLine();
+    void close();
+    FileReader(std::string);
+    ~FileReader();
+};
