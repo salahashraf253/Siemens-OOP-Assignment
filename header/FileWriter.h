a8f26c21e34ebf00e7d6a2f486ca11facd7249f0
Salah Ashraf <salahashraf924@gmail.com>
2023-02-07 02:45:39 +0200

    Create FileWriter.h

---
+++ header/FileWriter.h
@@ -0,0 +1,20 @@
+#include <fstream>
+#include"FileHandling.h"
+
+// #ifndef UNTITLED_FILEWRITER_H
+// #define UNTITLED_FILEWRITER_H
+
+class FileWriter:public FileHandling
+{
+private:
+    std::ofstream fileStream;
+
+public:
+    void open() override;
+    bool isGood() override;
+    void write(std::string);
+    void close() override;
+    FileWriter(std::string);
+    ~FileWriter();
+};
+// #endif 
