822930ccc133b21804ba6c1bc8a3fd5ac18b8056
Salah Ashraf <salahashraf924@gmail.com>
2023-02-07 02:45:46 +0200

    Create FileWriter.cpp

---
+++ source/FileWriter.cpp
@@ -0,0 +1,27 @@
+#include"../header/FileWriter.h"
+
+
+FileWriter::FileWriter(std::string fileName) : FileHandling(fileName)
+{
+
+}
+void FileWriter::open()
+{
+    std::string fileName=FileHandling::getFileName();
+    this->fileStream.open(fileName);
+}
+bool FileWriter::isGood()
+{
+    return this->fileStream.good();
+}
+void FileWriter::write(std::string lineToWrite)
+{
+    this->fileStream<<lineToWrite;
+}
+void FileWriter::close()
+{
+    this->fileStream.close();
+}
+FileWriter::~FileWriter()
+{
+}
