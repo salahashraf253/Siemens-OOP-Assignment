5659b6708649f977d870f453ad15060aacda8f1f
Salah Ashraf <salahashraf924@gmail.com>
2023-02-07 02:45:14 +0200

    Create FileHandling.h

---
+++ header/FileHandling.h
@@ -0,0 +1,21 @@
+#include <fstream>
+#include<string>
+
+// #ifndef UNTITLED_FILEHANDLING_H
+// #define UNTITLED_FILEHANDLING_H
+
+class FileHandling
+{
+private:
+    std::string fileName;
+
+public:
+    std::string getFileName();
+    virtual void open() = 0;
+    virtual bool isGood() = 0;
+    virtual void close() = 0;
+    FileHandling(std::string);
+    ~FileHandling();
+};
+
+// #endif 
