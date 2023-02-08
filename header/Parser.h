f98bfa002a263db30cc9edf21fb49d7e6bd0cf00
Salah Ashraf <salahashraf924@gmail.com>
2023-02-08 20:32:23 +0200

    Update Parser.h

--- header/Parser.h
+++ header/Parser.h
@@ -1,18 +1,22 @@
-#include <string>
-
 #ifndef UNTITLED_PARSER_H
 #define UNTITLED_PARSER_H
+
+#include <string>
 
 class Parser
 {
 protected:
     std::string rawPacket;
+
 public:
     std::string getDestinationAddress();
     std::string getSourceAddress();
     std::string getCRC();
     std::string getType();
-    Parser(std::string);
+    static std::string getType(std::string);
+    std::string getData();
+    void setRawPacket(std::string rawPacket);
+    virtual Frame *getParsedFrame() = 0;
     Parser();
     ~Parser();
 };
