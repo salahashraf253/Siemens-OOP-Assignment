f4d7063345011ef7602faa10211adcfa80e092f7
Salah Ashraf <salahashraf924@gmail.com>
2023-02-09 18:38:13 +0200

    fix: function 'writeParsedFrames' & reformat the code

--- source/Runner.cpp
+++ source/Runner.cpp
@@ -1,15 +1,6 @@
 #include "../header/Runner.h"
-#include<iostream>
+#include <iostream>
 
-Runner::Runner()
-{
-
-}
-Runner::~Runner()
-{
-    for(int i = 0; i < parsedFrames.size(); ++i)
-        delete parsedFrames[i];
-}
 
 void Runner::readFrames(std::string filename)
 {
@@ -17,45 +8,55 @@ void Runner::readFrames(std::string filename)
     fileToRead.open();
     while (fileToRead.isGood())
     {
-        std::string rawPacket=fileToRead.readLine();
+        std::string rawPacket = fileToRead.readLine();
         this->rawPackets.push_back(rawPacket);
     }
     fileToRead.close();
 
     parseFrames();
 }
-
-void Runner::parseFrames() {
-    ParserFactory factory;    
-    for(size_t i=0;i < rawPackets.size();i++)
+void Runner::parseFrames()
+{
+    ParserFactory factory;
+    for (int i = 0; i < rawPackets.size(); ++i)
     {
         Parser *unknownParser = factory.getParser(Parser::getType(rawPackets[i]));
-        unknownParser -> setRawPacket(rawPackets[i]);
-        Frame* parsedFrame = unknownParser -> getParsedFrame();
+        unknownParser->setRawPacket(rawPackets[i]);
+        Frame *parsedFrame = unknownParser->getParsedFrame();
         parsedFrames.push_back(parsedFrame);
         delete unknownParser;
     }
+    std::cout << this->parsedFrames.size() << "\n";
 }
 void Runner::writeParsedFrames(std::string filename)
-{      
+{
     FileWriter fileToWrite(filename);
     fileToWrite.open();
 
-    for(int i = 0; i < parsedFrames.size(); ++i) {
+    for (int i = 0; i < parsedFrames.size(); ++i)
+    {
         fileToWrite.write("Packet # " + std::to_string(i) + "\n");
-        fileToWrite.write(parsedFrames[i] -> toString());
-        if(i != parsedFrames.size() - 1) {
-            fileToWrite.write(std::string(150, '*') + "\n");
-        }
+        fileToWrite.write(parsedFrames[i]->toString());
+        fileToWrite.write("\n");
+        fileToWrite.write(std::string(150, '*') + "\n");
+        fileToWrite.write("\n");
     }
     fileToWrite.close();
 }
+void Runner::printParsedFrame()
+{
 
-void Runner::printParsedFrame() {
-
-    for(int i = 0; i < parsedFrames.size(); ++i) {
+    for (int i = 0; i < parsedFrames.size(); ++i)
+    {
         std::cout << "Packet # " << i << "\n";
-        std::cout << parsedFrames[i] -> toString();
-        std::cout << " -------------- \n"; 
+        std::cout << parsedFrames[i]->toString();
+        std::cout << " -------------- \n";
+    }
+}
+Runner::~Runner()
+{
+    for (int i = 0; i < parsedFrames.size(); ++i)
+    {
+        delete parsedFrames[i];
     }
 }
